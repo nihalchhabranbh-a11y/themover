44e89ca6fac29272142be995804a55c9fbe495e4
Apple <Apple@MacBook-Air.local>
2026-04-30 15:23:16 +0530

    Fix: Force HTTPS for all generated Cloudinary URLs globally

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ load_dotenv()
 cloudinary.config( 
   cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"), 
   api_key = os.getenv("CLOUDINARY_API_KEY"), 
-  api_secret = os.getenv("CLOUDINARY_API_SECRET") 
+  api_secret = os.getenv("CLOUDINARY_API_SECRET"),
+  secure = True
 )
 
 app = Flask(__name__)
